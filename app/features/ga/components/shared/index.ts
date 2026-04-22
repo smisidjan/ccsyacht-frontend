e3cb297a793d277374e65f1d58766470e4e7810e
Sarai Misidjan <Sarai.misidjan@hotmail.com>
2026-04-23 01:54:33 +0200

    fix: resolve Leaflet SSR error by removing direct exports
    Remove GAViewerWithDrawContent, GALeafletContent, GAPreviewMarker, and
    PinMarker from barrel export. These components use Leaflet which requires
    browser APIs and must only be imported dynamically with { ssr: false }.

--- app/features/ga/components/shared/index.ts
+++ app/features/ga/components/shared/index.ts
@@ -1,13 +1,13 @@
 /**
  * GA Shared Components
  * Barrel export for all shared GA UI components
+ *
+ * Note: Components that use Leaflet (GAViewerWithDrawContent, GALeafletContent,
+ * GAPreviewMarker, PinMarker) are NOT exported here because Leaflet requires
+ * browser APIs. They should only be imported dynamically with { ssr: false }.
  */
 
 export { default as GAViewer } from "./GAViewer";
 export { default as GAViewerWithDraw, type DeckBounds, type ExistingDeck } from "./GAViewerWithDraw";
-export { default as GAViewerWithDrawContent } from "./GAViewerWithDrawContent";
 export { default as GALeafletViewer } from "./GALeafletViewer";
-export { default as GALeafletContent } from "./GALeafletContent";
 export { default as GAPreview } from "./GAPreview";
-export { default as GAPreviewMarker } from "./GAPreviewMarker";
-export { default as PinMarker } from "./PinMarker";
